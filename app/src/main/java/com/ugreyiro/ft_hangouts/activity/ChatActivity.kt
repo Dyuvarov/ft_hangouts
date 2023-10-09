095622e628ba1863c510fc5f17afab08b909786c
Ullhor Greyiron <ugreyiro@paul-f4Ar2s5.clusters.42paris.fr>
2023-10-09 12:48:35 +0200

    create messages layouts

---
+++ app/src/main/java/com/ugreyiro/ft_hangouts/activity/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.ugreyiro.ft_hangouts.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ugreyiro.ft_hangouts.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
